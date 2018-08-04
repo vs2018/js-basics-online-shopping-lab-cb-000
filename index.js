002536a0bf4e4e7efc604293e318ef812fd8b9ac
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 10:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
 
-  // cart.length = 0
+  cart.length = 0
 
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
