ff4045d0e73e5aece5dc643de5daf03ad09a999d
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 09:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function setCart(c) {
   return cart;
 }
 
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
 function addToCart(item) {
   var cart = {}
   cart.itemName = item
-  cart.itemPrice = getRandomInt(0, 100)
+  cart.itemPrice = getRandomIntInclusive(0, 100)
   
 
   `${cart.itemName} has been added to your cart.`
