2d1cbd2f06f38e6a1e8e3395f5184c48c350f68f
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 09:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function getRandomIntInclusive(min, max) {
 function addToCart(item) {
   var cart = [];
   var obj = {};
-  obj.itemName = item;
-  obj.itemPrice = getRandomIntInclusive(0, 100);
+  obj['itemName'] = item;
+  obj['itemPrice'] = getRandomIntInclusive(0, 100);
   cart.push(obj);
 
   return `${item} has been added to your cart.`;
