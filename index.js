4ae7873be75a244652a42219e4d1ed9b5dd2c798
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 09:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if(item === cart[i]['itemName']) {
       cart.splice(i, 1)
-    } 
+    }
+    
   }
   return cart
 }
