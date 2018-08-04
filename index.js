38d9345f4bbbe3d0064414eea20347efcb6d6212
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 09:49:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
-    if(())
+    if(item === cart[i]['itemName'])
   }
 }
 
